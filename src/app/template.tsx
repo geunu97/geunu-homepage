7addea94591e2cb16caa57da95a63c5433689746
geunwoo <olo90632951@gmail.com>
2023-11-30 11:11:46 +0900

    feat: 푸터 추가

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,10 +1,12 @@
 import Header from '../../src/components/Header'
+import Footer from '../../src/components/Footer'
 
 export default function RootTemplate({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import styles from '../styles/footer.module.css'
+import Image from 'next/image'
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <Link href="https://github.com/geunu97" target="_blank">
+        <Image src="/images/github.png" alt="" width={20} height={20} />
+      </Link>
+      <Link href="mailto:olo90632951@gmail.com" target="_blank">
+        <Image src="/images/email.png" alt="" width={16} height={16} />
+      </Link>
+      <Link href="https://www.linkedin.com/in/geunu" target="_blank">
+        <Image src="/images/linkedin.png" alt="" width={20} height={20} />
+      </Link>
+    </div>
+  )
+}
