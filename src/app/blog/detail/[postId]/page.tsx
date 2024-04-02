7b63aca6d1ef5fb63a972165bb44764febc78e9e
geunu <olo90632951@gmail.com>
2024-04-02 18:09:04 +0900

    fix: force-dynamic 설정 추가

--- src/app/blog/detail/[postId]/page.tsx
+++ src/app/blog/detail/[postId]/page.tsx
@@ -1,6 +1,12 @@
 import { getBlogPost } from '@/api/blogPost';
 import PostDetail from '@/components/PostDetail';
 
+/* 이 페이지를 강제로 동적 컴포넌트로 처리 
+* 서버 컴포넌트는 기본적으로 SSG설정되어 있기 때문
+* firebase를 사용할 경우, fetch에서 no cache 옵션을 지정할 수 없기 때문
+*/
+export const dynamic = 'force-dynamic';
+
 interface BlogDetailPageProps {
   params: {
     postId: string;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,6 +5,12 @@ import styles from '@/styles/blogPage.module.css';
 import Image from 'next/image';
 import AuthGuardRender from '@/components/AuthGuardRender';
 
+/* 이 페이지를 강제로 동적 컴포넌트로 처리 
+* 서버 컴포넌트는 기본적으로 SSG설정되어 있기 때문
+* firebase를 사용할 경우, fetch에서 no cache 옵션을 지정할 수 없기 때문
+*/
+export const dynamic = 'force-dynamic';
+
 export default async function BlogPage() {
   const posts = await getBlogPosts();
 
